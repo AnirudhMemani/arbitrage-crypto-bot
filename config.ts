d015dfa41a067a85bb40ffcbc9e32f11d180951c
Anirudh Memani <nemanianirudh@gmail.com>
2024-03-22 05:03:12 +0530

    fixes

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-export const key = "46805304fe4fd45856574f8e458c6e650bd0cf54372a21f4";
+export const key = "6aeb8ffceb1bb2eec7e811f9e3ca81eda1a2a8db4366fdb7";
 export const secret =
-    "7c301b6a7146346102bbec26a682bcfe64373752f7afaa59e862ca83f990ba61";
+    "81c36300b536f4b61ed99f4702887518fbb44d1aa29a64505533744a8203ea52";
